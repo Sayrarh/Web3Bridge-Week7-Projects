d2bd5cba927934d7ad29a78970396a134b4dd112
Sayrarh <ajaotosinserah@gmail.com>
2022-10-14 12:20:52 +0100

    time

--- Auction/contracts/Auction.sol
+++ Auction/contracts/Auction.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
 
 /// @title Auction contract 
 /// @author Oluwatosin Serah Ajao
@@ -19,139 +19,142 @@ interface IERC721 {
 }
 
 contract Auction {
-    //My auction project's concept is to put an NFT up for bid, 
+    //My auction project's concept is to put an NFT up for Auction, 
     //with the winning bidder being the one who offers the highest price
 
     //events
     event Aunction(address caller, uint amountBidded);
-    event EndAunction(address indexed bidder, uint amount);
-    event withdrawBid(address bidder, uint bidAmount);
+    event Transfer(address highestBidder, uint bidAmount);
 
-    address payable owner;
-    //address of the owner of the NFT declared as payable to be able to receive the highest bidding price in ether
-    IERC721 immutable nftAddr; //address of the NFT
-    uint immutable tokenID; 
-    uint deadline = block.timestamp + 30 minutes; //Auction duration
-    uint public highestBid;//highest ether bidded
-    address public highestBidder; //address of the highest bidder
+    address public owner; //owner of the NFT
+    IERC721 immutable nftAddr; //address of the NFT contract
+    uint tokenID;
+    address[] biddersAddresses; //array of bidders Address
+    uint public highestBid;
+    address public highestBidder; //address of the current highest Bidder
+    uint deadline; //Auction duration
     bool started;
     bool ended;
 
     struct bidder{
         uint amountTobid;
         bool bidded;
+        bool  paid;
     }
 
+    mapping(address => bidder) biddersPrice;
 
-    address[] biddersAddresses;
-    mapping(address => bidder) public biddersBalance;
-   
-
-     modifier onlyOwner(){
+    modifier onlyOwner() {
         require(owner == msg.sender, "Not owner");
         _;
     }
 
-    modifier bidDuration() {
-        require(block.timestamp <= deadline, "Time has passed");
+    modifier timer() {
+       require(deadline <= block.timestamp, "Time has passed");
         _;
     }
 
-    constructor(IERC721 _nftAddr, uint _tokenID, uint _startingPrice){
-        owner = payable(msg.sender);
+    constructor (IERC721 _nftAddr, uint ID, uint _startingPrice) {
+        owner = msg.sender;
         nftAddr = _nftAddr;
-        tokenID = _tokenID;
+        tokenID = ID;
         highestBid = _startingPrice;
     }
 
-    //custom errors optimizes gas
-    error auctionStarted();
-    error auctionEnded();
+    error startedAuction();
 
-    //function to start aunction, owner should start this function
-    function startAuction() external onlyOwner{
+    ///@dev A function that allows owner to start the Auction
+    function startAuction(uint _setTime) external onlyOwner{
         if(started == true){
-           revert auctionStarted();
-        }
-     
+           revert ("Auction is in progress");
+         }else{
+             deadline = block.timestamp + _setTime;
+             require(deadline >= block.timestamp, "Auction has ended");
+         }
+         nftAddr.transferFrom(owner, address(this), tokenID);
         started = true;
     }
 
-    //function to bid NFT, anyone can bid but owner can't bid
-    function placeBid() external payable bidDuration {
-        bidder storage b = biddersBalance[msg.sender];
-        require(!b.bidded, "You have already bidded" );
-        require(msg.value > highestBid, "Bid more");
-        require(msg.sender != owner, "Owner can't bid");
-
-        if(msg.sender != address(0)){
-            b.amountTobid += msg.value;
-        }
+    /// @dev A function that allows bidders to place their bid
+    function placeBid(uint _amountTobid) external {
+        require(started == true, "Auction has not started");
+        require(block.timestamp <= deadline, "Time has passed");
+        require(msg.sender != owner, "NFT owner can't bid");
+        require(_amountTobid > highestBid, "Bid higher");
+        bidder storage bd = biddersPrice[msg.sender];
+        bd.amountTobid = _amountTobid;
+        highestBid = _amountTobid;
+       // biddersAddresses.push(msg.sender);
+        // for(uint i = 0; i < biddersAddresses.length; i++){
+        //     if(msg.sender != biddersAddresses[i]){
+        //         biddersAddresses.push(msg.sender);
+        //         continue ;
+        //     }
+        //}
         biddersAddresses.push(msg.sender);
 
-        highestBid = b.amountTobid;
         highestBidder = msg.sender;
-        
-        b.bidded = true;
-        emit Aunction(msg.sender, msg.value);
-
-    }
-//    
-    //function to withdraw, ether to be transferred to the bidder that didn't win
-    function withdraw() external{
-        require(ended, "We never ready!");
-        require(msg.sender != address(0), "Input a valid address");
-        require(biddersBalance[msg.sender].amountTobid > 0, "You didn't place a bid");
-         uint amount = biddersBalance[msg.sender].amountTobid;
-        //require (amount > 0, "You don't have any money to withdraw");
-         biddersBalance[msg.sender].amountTobid = 0; //to avoid reentrancy
-
-         payable(msg.sender).transfer(amount);
-         /*for(uint i = 0; i < biddersAddresses.length; i++){
-            if(msg.sender == biddersAddresses[i]){
-                 payable(msg.sender).transfer(amount);
-            }else{
-                revert("Not among the bidders");
-            }
-         }**/
-
-         emit withdrawBid(msg.sender, amount);
-
+        bd.bidded = true;
+        emit Aunction(msg.sender, _amountTobid);
     }
 
-
-    //function to end aunction, NFT should be transferred from the contract to the highest bidder here
-    function endAunction() external onlyOwner{
-
-         if(ended == true){
-            revert auctionEnded();
-        }
-
-       // require(ended == false, "Auction already ended!");
-
-        if(highestBidder != address(0)){
-            nftAddr.safeTransferFrom(address(this), highestBidder, tokenID);
-            payable(owner).transfer(highestBid);
-        }else{
-            nftAddr.safeTransferFrom(address(this), owner, tokenID);
-        }
+    /// @dev function that allows highest bidder to deposit his bid
+    function deposit() external payable{
+        require(biddersPrice[msg.sender].amountTobid > 0, "You didn't bid");
+        require(highestBidder == msg.sender, "You are not the winner");
+        require(msg.value == highestBid, "Input the exact higest bid");
+        bidder storage bd = biddersPrice[msg.sender];
+        highestBid = msg.value;
+        bd.paid = true;
         ended = true;
+        emit Transfer(highestBidder, msg.value);
+    }
 
-        emit EndAunction(highestBidder, highestBid);
+    ///@dev function to transfer the NFT to the highest bidder and transfer the higest bid to the owner
+    function transfer() external payable{
+            require(started == true, "Auction has not started");
+            require(ended == true, "Auction is still in progress");
+            require(deadline <= block.timestamp, "Not time for withdrawal");
+            require(highestBidder == msg.sender || msg.sender == owner , "Only winner or NFT owner");
+            bidder memory bd = biddersPrice[msg.sender];
+            if(bd.paid == true){
+            nftAddr.safeTransferFrom(address(this), highestBidder, tokenID);
+            payable(owner).transfer(address(this).balance); //Work on the bal
+           }
+           else{
+           nftAddr.safeTransferFrom(address(this), owner, tokenID);
+        }
+       
     }
     
-    //function to get contract balance
+     /// @dev function to show current reading time
+     function currentTime() public view returns(uint){
+         if(deadline == 0){
+             return 0;
+         }else{
+             return deadline - block.timestamp; 
+         }
+        
+     }
+    
+    /// @dev A function to display bidders bidding price
+    function showBidderPrice() external view returns(uint amountBidded){
+       bidder memory bd = biddersPrice[msg.sender]; 
+       amountBidded = bd.amountTobid ;
+    }
+
+     
+    /// @dev function to get contract balance
     function getContractBalance() public view returns(uint bal){
         bal = address(this).balance;
     }
 
-    //function to return bidders addresses
-    function showBiddersAddr() public view returns(address[] memory){
+    /// @dev Function to display all bidders addresses
+    function showAllBidders() external view returns(address[] memory){
         return biddersAddresses;
     }
 
     receive() external payable{}
 
-   
     
 }
